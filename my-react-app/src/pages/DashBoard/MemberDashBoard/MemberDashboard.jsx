f0f3c9885ed05ce90f8fb5a98b69816568eef773
Kanchanamkl <92777840+Kanchanamkl@users.noreply.github.com>
2024-11-17 09:29:20 +0530

    member dashboard changes

--- my-react-app/src/pages/DashBoard/MemberDashBoard/MemberDashboard.jsx
+++ my-react-app/src/pages/DashBoard/MemberDashBoard/MemberDashboard.jsx
@@ -1,14 +1,132 @@
 import React, { useState, useEffect } from "react";
 import "./MemberDashboardStyles.scss";
+import { GrFormPrevious, GrFormNext } from "react-icons/gr";
+import CoachCard from "../../../components/CoachCard/CoachCard";
+import SectionContainer from "../../../components/SectionContainer/SectionContainer";
+import CourtCard from "../../../components/CourtCard/CourtCard";
 
 const MemberDashboard = () => {
-    return (
-        <>
-            <div className="member-dashboard-container">
-                <h1>Member DashBoard</h1>
+  const [featuredCoaches, setFeaturedCoaches] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const coachesPerPage = 4;
+
+  const [featuredCourts, setFeaturedCourts] = useState([]);
+  const [currentCourtIndex, setCurrentCourtIndex] = useState(0);
+  const courtsPerPage = 4;
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/users/get-all-coaches")
+      .then((response) => response.json())
+      .then((data) => setFeaturedCoaches(data))
+      .catch((error) => console.error("Error fetching coaches:", error));
+  }, []);
+
+  const nextCoach = () => {
+    if (currentIndex + coachesPerPage < featuredCoaches.length) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const previousCoach = () => {
+    if (currentIndex != 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const currentCoaches = featuredCoaches.slice(
+    currentIndex,
+    currentIndex + coachesPerPage
+  );
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/court-bookings/courts")
+      .then((response) => response.json())
+      .then((data) => setFeaturedCourts(data))
+      .catch((error) => console.error("Error fetching coaches:", error));
+  }, []);
+
+  const nextCourt = () => {
+    if (currentCourtIndex + coachesPerPage < featuredCourts.length) {
+      setCurrentCourtIndex(currentIndex + 1);
+    }
+  };
+
+  const previousCourt = () => {
+    if (currentCourtIndex != 0) {
+      setCurrentCourtIndex(currentCourtIndex - 1);
+    }
+  };
+
+  const currentCourts = featuredCourts.slice(
+    currentCourtIndex,
+    currentCourtIndex + courtsPerPage
+  );
+
+  return (
+    <>
+      <div className="member-dashboard-container">
+        <div className="featured-coaches">
+          <SectionContainer title="Featured Coaches">
+            <div className="coach-list">
+              {/* Previous Button */}
+              <div
+                onClick={previousCoach}
+                className={`pagination-icon left-icon ${
+                  currentIndex === 0 ? "disabled" : ""
+                }`}
+              >
+                <GrFormPrevious size={30} cursor={"pointer"} />
+              </div>
+              {currentCoaches.map((coach) => (
+                <CoachCard key={coach.id} coach={coach} />
+              ))}
+              {/* Next Arrow Icon */}
+              <div
+                onClick={nextCoach}
+                className={`pagination-icon right-icon ${
+                  currentIndex + coachesPerPage >= featuredCoaches.length
+                    ? "disabled"
+                    : ""
+                }`}
+              >
+                <GrFormNext size={30} cursor={"pointer"} />
+              </div>
             </div>
-        </>
-    );
+          </SectionContainer>
+        </div>
+
+        <div className="featured-courts">
+          <SectionContainer title="Featured Courts">
+            <div className="coach-list">
+              {/* Previous Button */}
+              <div
+                onClick={previousCourt}
+                className={`pagination-icon left-icon ${
+                  currentIndex === 0 ? "disabled" : ""
+                }`}
+              >
+                <GrFormPrevious size={30} cursor={"pointer"} />
+              </div>
+              {currentCourts.map((court) => (
+                <CourtCard key={court.id} court={court} />
+              ))}
+              {/* Next Arrow Icon */}
+              <div
+                onClick={nextCourt}
+                className={`pagination-icon right-icon ${
+                  currentCourtIndex + courtsPerPage >= featuredCourts.length
+                    ? "disabled"
+                    : ""
+                }`}
+              >
+                <GrFormNext size={30} cursor={"pointer"} />
+              </div>
+            </div>
+          </SectionContainer>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MemberDashboard;
