f629003e95e72fe696962bc00c4f959d6bd3695a
Kanchanamkl <92777840+Kanchanamkl@users.noreply.github.com>
2024-11-29 18:54:53 +0530

    coach sesstion booking card changes

--- my-react-app/src/components/BookingCard/CoachBookingCard/CoachBookingCard.jsx
+++ my-react-app/src/components/BookingCard/CoachBookingCard/CoachBookingCard.jsx
@@ -2,34 +2,38 @@
 import React, { useState, useEffect } from "react";
 import "./CoachBookingCardStyles.scss";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { StoreContext } from "../../../StoreContext/StoreContext";
 const CoachBookingCard = ({ coach_booking }) => {
 
-  // useEffect(() => {
-  //   console.log('coach booking:',coach_booking);
-  // }, [coach_booking]);
+
+  const { role } = useContext(StoreContext);
+  const userRole = role;
+
+  useEffect(() => {
+    console.log('coach booking:',coach_booking);
+  }, [coach_booking]);
   return (
-    <div className="coach-booking-card-container">
-      <div className="client_coach_booking_card" key={coach_booking.id}>
+    <div key={coach_booking.id} className="coach-booking-card-container">
+      {/* <div className="" > */}
         <div className="profile-img">
           {console.log('coach booking:',coach_booking.id)}
-          {coach_booking.coach.profilePic && (
+
+          {coach_booking[userRole === 'MEMBER' ? 'coach' : 'member'].profilePic && (
             <img
-              src={coach_booking.coach.profilePic }
-              alt={`${coach_booking.coach.profilePic}`}
+              src={coach_booking[userRole === 'MEMBER' ? 'coach' : 'member'].profilePic}
+              alt={`${coach_booking[userRole === 'MEMBER' ? 'coach' : 'member'].profilePic}`}
             />
           )}
-          {/* <img
-            src={coach_booking.coach.user.profilePic}
-            alt={coach_booking.coach.user.profilePic}
-          /> */}
         </div>
         <div className="details">
-          <h4 className="coach-name">Coach : {coach_booking.coach.firstName} {coach_booking.coach.lastName}</h4>
+          <h4 className="coach-name">
+            {userRole === 'MEMBER' ? 'Coach' : 'Member'} : {coach_booking[userRole === 'MEMBER' ? 'coach' : 'member'].firstName} {coach_booking[userRole === 'MEMBER' ? 'coach' : 'member'].lastName}
+          </h4>
           <p className="coach_booking-date">Date : {coach_booking.date}</p>
           <p className="coach_booking-time">Time : {coach_booking.startTime} : {coach_booking.endTime}</p>
-
         </div>
-      </div>
+      {/* </div> */}
     </div>
   );
 };
